a863049652cfd8cba0bce2be91585c1b238f8ac0
Vatsal Agarwal <bindalvatsaljpr@gmail.com>
2023-09-27 16:23:15 +0530

    changes saved

---
+++ src/Components/BarGraphfirst.jsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import { Chart as ChartJS, BarElement, CategoryScale, LinearScale, Category, Tooltip, Legend } from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+import PieChart from './PieChart';
+import BarGraphsecond from './BarGraphsecond';
+
+
+ChartJS.register(
+  BarElement, CategoryScale, LinearScale, Tooltip, Legend
+)
+
+function BarGraphfirst() {
+  const data = {
+    labels: ['1st Qtr.', '2nd Qtr.', '3rd Qtr.', '4th Qtr.'],
+    datasets: [
+      {
+        data: [70, 30, 60, 90],
+        backgroundColor: '#FDB21C',
+        barThickness: 50,
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      y: {
+        beginAtZero: true,
+        max: 100,
+        ticks: {
+          stepSize: 20,
+          callback: function (value) {
+            return value + '%';
+          },
+        },
+        grid: {
+          borderDash: [4, 4],
+        },
+      },
+      x: {
+        grid: {
+          display: false,
+        },
+      },
+    },
+    plugins: {
+      legend: {
+        display: false,
+      },
+      tooltip: {
+        callbacks: {
+          label: function (context) {
+            return context.dataset.data[context.dataIndex] + '%';
+          },
+        },
+      },
+    },
+  }
+  return (
+    <div className="BarGraphfirst" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <h1>Some Graph Examples</h1>
+      <div style={{ display: 'flex', width: '90%', justifyContent: 'center' }}>
+        <div style={{ flex: '50%' }}> {/* Equal width for pie chart and bar graph */}
+          <PieChart />
+        </div>
+        <div style={{ flex: '50%' }}> {/* Equal width for pie chart and bar graph */}
+          <h3 style={{ margin: '20px' }}>Average Inventory Turns</h3>
+          <div style={{
+            padding: '20px',
+            width: '100%',
+            height: '250px',
+          }}>
+            <Bar
+              data={data}
+              options={options}
+            ></Bar>
+          </div>
+        </div>
+      </div>
+      <BarGraphsecond />
+    </div>
+  )
+}
+
+export default BarGraphfirst;
